b86f74441d3f8d47c72b331d9182465eb571c141
Eduardo Almeida dos Santos <durdo.one@gmail.com>
2017-03-26 13:57:13 -0300

    Improving readability

--- getSpells.py
+++ getSpells.py
@@ -11,16 +11,21 @@ class playerClass:
         self.classSpellList = []
         self.tree = None
         self.createClassSpellList()
+
     def getName(self):
         return self.name
+
     def getUrl(self):
         return self.url
+
     def getClassSpellList(self):
         return self.classSpellList
+
     def createClassSpellList(self):
         self.getTree()
         self.classSpellList = self.tree.xpath('//div[@class="wrapper"]/ul/li/a/text()')
         print(self.classSpellList)
+
     def getTree(self):
         self.tree = lh.parse(urlopen(self.url)) if self.tree == None else self.tree
         return self.tree
@@ -32,14 +37,19 @@ class spell:
         self.url = spellUrl
         self.level = ""
         self.tree = None
+
     def getName(self):
         return self.name
+
     def getUrl(self):
         return self.url
+
     def getLevel(self):
         return self.level
+
     def setLevel(self, spellLevel):
         self.level = spellLevel
+
     def getTree(self):
         self.tree = lh.parse(urlopen(self.url)) if self.tree == None else self.tree
         return self.tree
@@ -50,25 +60,32 @@ class main:
     classesList = []
     spellLevels = []
     fullSpellList = []
+
     @staticmethod
     def __init__(page):
         main.pageUrl = page
+
     @staticmethod
     def getPage():
         return main.pageUrl
+
     @staticmethod
     def getTree():
         main.tree = lh.parse(urlopen(main.pageUrl)) if main.tree == None else main.tree
         return main.tree
+
     @staticmethod
     def getClassesList():
         return main.classesList
+
     @staticmethod
     def getFullSpellList():
         return main.fullSpellList
+
     @staticmethod
     def getsSpellRanking():
         return main.spellLevels
+
     @staticmethod
     def createClassList():
         main.getTree()
@@ -76,10 +93,12 @@ class main:
         ur = [urljoin(main.pageUrl,"".join(main.tree.xpath('//a[text()="%s"]/@href' % c))) for c in cl]
         for count, curClass in enumerate(cl):
             main.classesList.append(playerClass(curClass, ur[count]))
+
     @staticmethod
     def createLevelList():
         main.getTree()
         main.spellLevels = main.tree.xpath('//div[@class="home"]/h2/text()')
+
     @staticmethod
     def createSpellList():
         main.getTree()
@@ -87,6 +106,7 @@ class main:
         ur = [urljoin(main.pageUrl,"".join(main.tree.xpath('//a[text()="%s"]/@href' % c))) for c in cl]
         for count, curClass in enumerate(cl):
             main.fullSpellList.append(spell(curClass, ur[count]))
+
     @staticmethod
     def sortFullSpellList():
         main.fullSpellList.sort(key=lambda spell: spell.name)
@@ -96,10 +116,3 @@ main.createClassList()
 main.createLevelList()
 main.createSpellList()
 main.sortFullSpellList()
-#print(main.spellLevels)
-#print("Class list:")
-#print("\n".join(["'" + element.getName() + "' at --> " + element.getUrl() for element in main.classesList]))
-#print("\n\nSpell list:")
-#print("\n".join(["'" + element.getName() + "' at --> " + element.getUrl() for element in main.fullSpellList]))
-#[print("\nClass '%s' can be found at:\n%s" % (c.getName(), c.getUrl())) for c in main.getClassesList()]
-#print("\nSpells are divided in the following levels:")
